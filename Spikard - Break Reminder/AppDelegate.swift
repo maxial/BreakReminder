8da02ddc92d67ef0160ac902cf3f26d91bbe85fc
maxial <alievmaxx@gmail.com>
2019-05-25 22:58:03 +0300

    Initial Commit

---
+++ Spikard - Break Reminder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spikard - Break Reminder
+//
+//  Created by maxial on 25/05/2019.
+//  Copyright © 2019 Spikard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Spikard - Break Reminder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spikard - Break Reminder
+//
+//  Created by maxial on 25/05/2019.
+//  Copyright © 2019 Spikard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
